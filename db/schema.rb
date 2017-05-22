3b286dc8e792bfb88e5045622ff0123f0c2ecd1f
dayudodo <dayudodo@gmail.com>
2017-05-22 18:01:31 +0800

    add postgres in compose

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170519094047) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tests", force: :cascade do |t|
     t.string "title"
     t.string "content"
