405488d5788af82fe9a0cc869d2cd395636b77ed
dayudodo <dayudodo@gmail.com>
2017-06-02 15:41:56 +0800

    test.sqlite3 local

--- test/system/titles_test.rb
+++ test/system/titles_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class TitlesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit titles_url
-  #
-  #   assert_selector "h1", text: "Title"
-  # end
+  test "visiting the index" do
+    visit titles_url
+  
+    assert_selector "h1", text: "Title"
+  end
 end
